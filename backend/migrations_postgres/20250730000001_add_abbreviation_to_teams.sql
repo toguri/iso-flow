06291b772ab863500ebed00bbffde81b8b3043f8
toguri <oguri_toru@cyberagent.co.jp>
2025-07-30 20:48:41 +0900

    fix: teamsテーブルのabbreviationカラムを初期定義に追加
    - CIエラーを修正
    - 不要なマイグレーションファイルを削除
    - teamsテーブルの初期定義にabbreviationカラムを追加

--- backend/migrations_postgres/20250730000001_add_abbreviation_to_teams.sql
+++
@@ -1,8 +0,0 @@
--- teamsテーブルにabbreviationカラムを追加
-ALTER TABLE teams ADD COLUMN IF NOT EXISTS abbreviation TEXT;
-
--- 既存データの更新
-UPDATE teams SET abbreviation = id WHERE abbreviation IS NULL;
-
--- NOT NULL制約を追加
-ALTER TABLE teams ALTER COLUMN abbreviation SET NOT NULL;
